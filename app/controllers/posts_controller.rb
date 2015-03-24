b842fd16cceeb2e8cc4f1f936018dabb46ed551f
rafael <rafael.adel20@gmail.com>
2015-03-24 17:08:12 +0200

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_url
+    else
+      render 'posts/new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_url
+    else
+      render 'posts/edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_url
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
---
+++ test/controllers/posts_controller_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class PostsControllerTest < ActionController::TestCase
+  test "posts index" do
+    get :index
+    assert_template "posts/index"
+    assert_select 'div.post', 2
+  end
+
+  test "get new post" do
+    get :new
+    assert_template 'posts/new'
+    assert_select 'form[action=?]', posts_path
+    assert_select 'input[type=submit]', value: "Add"
+  end
+
+  test "post new post" do
+    assert_difference 'Post.count', 1 do
+      post :create, post: {title: "new title", content: "new content"}
+    end
+    assert_redirected_to posts_url
+  end
+
+  test "edit post" do
+    first_post = posts(:first)
+    title = "www"
+    content = "ccc"
+    patch :update, id: first_post, post: {title: title, content: content }
+    first_post.reload
+    assert_equal first_post.title, title
+    assert_equal first_post.content, content
+  end
+
+  test "delete post" do
+    first_post = posts(:first)
+    assert_difference "Post.count", -1 do
+      delete :destroy, id: first_post
+    end
+    assert_redirected_to posts_url
+  end
+end
