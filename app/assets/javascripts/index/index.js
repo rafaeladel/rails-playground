4d70cbd6b7f8158e0fac43a62bf2c62736206bc2
rafael <rafael.adel20@gmail.com>
2015-03-26 17:39:52 +0200

    ajax

--- app/assets/javascripts/index/index.js
+++ app/assets/javascripts/index/index.js
@@ -1,18 +1,18 @@
-$("body").on("click", ".new_post input[type=submit]", function (e) {
-    e.preventDefault();
-   var form = $(e.target).closest("form"),
-       url = form.attr("action"),
-       form_data = new FormData(form.get(0));
-
-    $.ajax({
-        type: "POST",
-        url: url,
-        dataType: "json",
-        data: form_data,
-        contentType: false,
-        processData: false,
-        success: function (data) {
-            console.log(data);
-        }
-    });
-});
+//$("body").on("click", ".new_post input[type=submit]", function (e) {
+//    e.preventDefault();
+//   var form = $(e.target).closest("form"),
+//       url = form.attr("action"),
+//       form_data = new FormData(form.get(0));
+//
+//    $.ajax({
+//        type: "POST",
+//        url: url,
+//        dataType: "json",
+//        data: form_data,
+//        contentType: false,
+//        processData: false,
+//        success: function (data) {
+//            console.log(data);
+//        }
+//    });
+//});
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
       # render json: @posts
       respond_to do |format|
         format.html { redirect_to posts_url}
-        format.json { render json: @posts }
+        format.js
       end
     else
       respond_to do |format|
